3debd4abe3fef9d1d5073dfcdf050a3cf68fee43
djx <jxce_d@163.com>
2017-07-30 18:18:35 +0800

    ok

---
+++ Web/Areas/Admin/Controllers/UserInfoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Entity;
+
+namespace Web.Areas.Admin.Controllers
+{
+    public class UserInfoesController : Controller
+    {
+        private businessSiteEntities db = new businessSiteEntities();
+
+        // GET: Admin/UserInfoes
+        public ActionResult Index()
+        {
+            return View(db.UserInfo.ToList());
+        }
+
+        // GET: Admin/UserInfoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfo.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // GET: Admin/UserInfoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/UserInfoes/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserId,UserName,UserPwd,RealName,UserTelphone,UserEmail,UserIsLock,UserAddTime")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserInfo.Add(userInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userInfo);
+        }
+
+        // GET: Admin/UserInfoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfo.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // POST: Admin/UserInfoes/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserId,UserName,UserPwd,RealName,UserTelphone,UserEmail,UserIsLock,UserAddTime")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userInfo).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userInfo);
+        }
+
+        // GET: Admin/UserInfoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfo.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // POST: Admin/UserInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserInfo userInfo = db.UserInfo.Find(id);
+            db.UserInfo.Remove(userInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
