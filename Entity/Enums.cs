9340adb515dd8c65549de21788167c3ce50d1fad
djx <jxce_d@163.com>
2017-08-03 01:12:11 +0800

    ok

---
+++ Entity/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Enums
+    {
+        public enum EStatus
+        {
+            normal=0,
+            stop=1
+        }
+    }
+}
---
+++ Web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Entity;
+
+namespace Web.Areas.Admin.Controllers
+{
+    public class DashboardController : BaseController
+    {
+        // GET: Admin/Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Menus()
+        {
+            
+            base.MenusBLL.Where(c => c.MenusStatus == ((int)Enums.EStatus.normal == 0 ? false : true));
+            return PartialView();
+        }
+    }
+}
