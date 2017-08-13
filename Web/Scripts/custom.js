2d372df0e977b141e6ee388b842bccac5e22e6db
djx <jxce_d@163.com>
2017-08-13 14:29:55 +0800

    ok

---
+++ Web/Scripts/custom.js
@@ -0,0 +1,14 @@
+﻿    function categroy_operations(switch_on) {
+        switch (switch_on) {
+            case 'create':
+                window.location.href = "/Admin/Dashboard/Category_Create";
+                break;
+            case 'del':
+                break;
+            case 'edit':
+                break;
+            default:
+                alert("失败");
+                break;
+        }
+    }
